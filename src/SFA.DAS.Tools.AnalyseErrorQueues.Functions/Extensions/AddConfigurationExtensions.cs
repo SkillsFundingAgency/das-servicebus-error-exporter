a1f16008caace25d452511d059a9c8b3b0652000
Karen Hanna <karen@laveryhanna.com>
2025-01-30 14:10:48 +0000

    upgrade to net 8

---
+++ src/SFA.DAS.Tools.AnalyseErrorQueues.Functions/Extensions/AddConfigurationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+using System;
+using System.IO;
+
+namespace SFA.DAS.Tools.AnalyseErrorQueues.Functions.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static FunctionsApplicationBuilder AddConfiguration(this FunctionsApplicationBuilder builder)
+        {
+            var basePath = Directory.GetCurrentDirectory();
+            var filePath = Path.Combine(basePath, "local.settings.json");
+
+            builder.Configuration
+                .SetBasePath(basePath)
+                .AddJsonFile(filePath, optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            builder.Build();
+            var config = builder.Configuration;
+
+            builder.Configuration.AddAzureTableStorage(options =>
+            {
+                options.ConfigurationKeys = config["ConfigNames"]?.Split(",") ?? Array.Empty<string>();
+                options.StorageConnectionString = config["ConfigurationStorageConnectionString"];
+                options.EnvironmentName = config["EnvironmentName"];
+                options.PreFixConfigurationKeys = false;
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ src/SFA.DAS.Tools.AnalyseErrorQueues.Infrastructure/ApplicationConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Tools.AnalyseErrorQueues.Functions.Infrastructure
+{
+    public class ApplicationConfiguration
+    {
+        public BlobDataSinkSettings BlobDataSinkSettings { get; set; }
+        
+        public ServiceBusRepoSettings ServiceBusRepoSettings { get; set; }
+       
+        public LADataSinkSettings LADataSinkSettings { get; set; }
+    }
+}
